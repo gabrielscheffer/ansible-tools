61e58883ff07dc5e75db06f6716399cd94ed6986
Gabriel Scheffer de Souza <o_gabriel@outlook.com>
2016-06-09 20:51:40 -0300

    Improvement on docker garbage collection

--- roles/docker-config/files/docker_cleanup.sh
+++ roles/docker-config/files/docker_cleanup.sh
@@ -2,5 +2,6 @@
 echo Limpeza Docker
 docker ps -q -a |xargs docker rm
 echo Containers parados removidos
-docker rmi $(docker images | grep '^<none>' | awk '{print $3}')
+docker rmi $(docker images -a | grep "^<none>" | awk '{print $3}')
+docker rmi $(docker images -f "dangling=true" -q)
 echo Imagens de containers parados removidas
