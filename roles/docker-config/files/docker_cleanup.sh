d7d523fc012dae76becd96821f12b2b03dfa06ab
gabrielscheffer <o_gabriel@outlook.com>
2016-10-11 11:56:20 -0300

    Improvement docker_cleanup

--- roles/docker-config/files/docker_cleanup.sh
+++ roles/docker-config/files/docker_cleanup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 echo Limpeza Docker
-docker ps -q -a |xargs docker rm
+docker ps -q -a |xargs docker rm > /dev/null 2>&1
 echo Containers parados removidos
 dockerIDs=$(docker images -a | grep "^<none>" | awk '{print $3}')
 if [[ $dockerIDs ]]; then
-    docker rmi $dockerIDs
+    docker rmi $dockerIDs > /dev/null 2>&1
 fi
-dockerIDs=$(docker images -f "dangling=true" -q)
-if [[ $dockerIDs ]]; then
-    docker rmi $dockerIDs
-fi
+dockerIDs=$(docker images -q)
+docker rmi $dockerIDs > /dev/null 2>&1
 echo Imagens de containers parados removidas
